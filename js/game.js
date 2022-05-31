1cb6e30d356ab90906dbc79188dfd09b1fa1cd53
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-05-31 15:04:16 +0000

    started file

---
+++ js/game.js
@@ -0,0 +1,30 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Brayden MacMillan All rights reserved
+//
+// Created by: Brayden MacMillan
+// Created on: May 2022
+// This is the Phaser3 configueration file
+
+//* Game scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
